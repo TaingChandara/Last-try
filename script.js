aa1f71c2700b919eae25500c29fde2e58aa6dcc6
TaingChandara <118673484+TaingChandara@users.noreply.github.com>
2022-11-30 10:47:17 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+// Truthy / Faslsy Value
+
+console.log(Boolean(0));
+console.log(Boolean(undefined));
+console.log(Boolean('Jonas'));
+console.log(Boolean({}));
+console.log(Boolean(''));
+const money = 100;
+if (money) {
+    console.log("DOnt't spend it all ;)");
+} else {
+    console.log('You should get a job!');
+}
+
+let height = 1;
+if (height) {
+    console.log('YAY! Height is defined');
+} else {
+    console.log('Height is UNDERFINED ');
+}
